5e8326dffe17bccf624d779926fa082e8d0c740e
Jack Dempsey <jack.dempsey@gmail.com>
2008-09-05 17:43:17 -0400

    set gists as a resource and default to its index action

--- config/router.rb
+++ config/router.rb
@@ -22,7 +22,7 @@
 Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do |r|
   # RESTful routes
-  # r.resources :posts
+  r.resources :gists
 
   # This is the default route for /:controller/:action/:id
   # This is fine for most cases.  If you're heavily using resource-based
@@ -31,5 +31,5 @@ Merb::Router.prepare do |r|
   r.default_routes
   
   # Change this for your home page to be available at /
-  # r.match('/').to(:controller => 'whatever', :action =>'index')
+  r.match('/').to(:controller => 'gists', :action =>'index')
 end
