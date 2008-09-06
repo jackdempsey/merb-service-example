44971ef170b8e97cfa5ce0707d61733d67937525
Jack Dempsey <jack.dempsey@gmail.com>
2008-09-06 02:18:56 -0400

    clean up a bit; add spec post

--- spec/controllers/gists_spec.rb
+++ spec/controllers/gists_spec.rb
@@ -1,13 +1,22 @@
 require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
-describe Gists, "/index" do
+describe Gists, "/" do
 
   def do_get
-    get '/gists/index.json'
+    get '/gists.json'
   end
-  
+
+  def do_post(params)
+    post '/gists.json', {:gist => params}
+  end
+
   it "should respond successfully to a GET of /index.json" do
     do_get.should be_successful
+  end
+
+  it "should respond successfully to a POST with good data" do
+    controller = do_post :url => 'foo'
+    controller.should be_successful
   end
 
 end
