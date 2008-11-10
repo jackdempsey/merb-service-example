5b67b0f51449969e6b13566356ae22a7f83d5b7e
Jack Dempsey <jack.dempsey@gmail.com>
2008-11-10 16:05:30 -0500

    bring example up to 1.0

--- app/controllers/exceptions.rb
+++ app/controllers/exceptions.rb
@@ -1,31 +1,13 @@
-class Exceptions < Application
-  provides :json
-
-  # handle BadRequest exceptions (400)
-  def bad_request
-    render_nil
-  end
-
+class Exceptions < Merb::Controller
+  
   # handle NotFound exceptions (404)
   def not_found
-    render_nil
+    render :format => :html
   end
 
   # handle NotAcceptable exceptions (406)
   def not_acceptable
-    render_nil
+    render :format => :html
   end
 
-  # handle OK exceptions (200)
-  def ok
-    render_nil
-  end
-
-  private
-
-  # need a clean way to render nothing and return a status
-  # for now we use this little wrapper method.
-  def render_nil
-    render ''
-  end
 end
