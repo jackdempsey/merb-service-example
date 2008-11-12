1f49d2d40ec2e0077e2736a80897e30b189f1103
Jack Dempsey <jack.dempsey@gmail.com>
2008-11-12 01:45:56 -0500

    borrow from http://www.theamazingrando.com/blog/?p=46 and rework exception handling

--- app/controllers/exceptions.rb
+++ app/controllers/exceptions.rb
@@ -1,15 +1,27 @@
 class Exceptions < Merb::Controller
+  provides :json
   
   # handle NotFound exceptions (404)
   def not_found
     # look into use params[:format] if you want to display html and json
+    return standard_error if content_type == :json
     render :format => :json
   end
 
   # handle NotAcceptable exceptions (406)
   def not_acceptable
     # look into use params[:format] if you want to display html and json
+    return standard_error if content_type == :json
     render :format => :json
+  end
+
+  def standard_error
+    # Re-Raise so we get the pretty merb error document instead.
+    raise request.exceptions.first if content_type == :html
+
+    @exceptions = request.exceptions
+    @show_details = Merb::Config[:exception_details]
+    render :standard_error
   end
 
 end
